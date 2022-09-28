c3c7dc3ec38cce328c27705ce0b35be84c96cb32
AriyanShihab <devshihab111@gmail.com>
2022-09-28 21:34:16 +0600

    install tailwind and created fake data

---
+++ src/requerments/requerments.js
@@ -0,0 +1,46 @@
+/*
+
+
+1: website must have diffarent name, and must be showed in browser title;
+2: we can use any relative logos
+3: style as I wish ( same style not recomended)
+4: have to create some face data and tese four informotion must be in the data
+    a:image (related to website better to use same size images)
+    b:must have a name;
+    c: how many time does ot take
+    d:  add to to list button
+
+5: minimum 6 facke data is needed
+6: use any CSS framework
+7: show some inforamtion about my self on the top right side
+8: add a breack , minimum 4 button nedded
+9: make the site responsive
+
+
+INTERACTION'S
+   1: when we click a card button it will pass the time to take time area and it will add with previous take time vaule (defualt value is 0)
+   2: when ever we lick any button inside add a breack , it will pass that time into breck time and replace the time, (mo mathemitical culculation nedded)
+
+
+FOR BONOUS:
+    1: break time will save on local storege, and display it after the relod of the page
+    2:
+    3: folder strucher
+    4: keys in map
+    5:10 commit (minimum)
+    6: display a toast when clicked on activiti cpmplete tak button
+
+THREE QUESTION TO ANSWER
+     1: how does react work
+     2: diffarent between props and state
+     3: use of useEffect (except fetch data)
+
+
+
+
+
+
+
+
+
+*/
